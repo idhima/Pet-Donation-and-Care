11c0b56c52c72a69c7cec0e816b67fd2872b1046
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-06 19:17:28 +0530

    Create src-App.js

---
+++ Frontend/src-App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PetPage from './pages/PetPage';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/pets" element={<PetPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
