4a60c936a2f074dcfe806d757dd4c251ba38a58c
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-06 19:29:23 +0530

    Update src-App.js

--- Frontend/src-App.js
+++ Frontend/src-App.js
@@ -4,18 +4,31 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PetPage from './pages/PetPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { UserProvider } from './context/UserContext';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/pets" element={<PetPage />} />
-            </Routes>
-        </Router>
+        <UserProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route
+                        path="/pets"
+                        element={
+                            <ProtectedRoute>
+                                <PetPage />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </Router>
+        </UserProvider>
     );
 }
+
+export default App;
 
 export default App;
